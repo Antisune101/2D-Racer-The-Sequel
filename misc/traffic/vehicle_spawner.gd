3b64a04706c0bfc1596e1f2336ebb563a2a768ba
Antisune101 <ewanbester72@gmail.com>
2024-06-11 09:53:35 +0200

    IDK why I'm trying to make this nice anyway

--- misc/traffic/vehicle_spawner.gd
+++ misc/traffic/vehicle_spawner.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 	pass
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_select"):
 		spawn_vehicle()
 
@@ -19,4 +19,3 @@ func spawn_vehicle() -> void:
 	var new_vehicle: Vehicle = vehicle_scene.instantiate()
 	movement_path.add_child(new_vehicle)
 	new_vehicle.init_vehicle(vehicle_database.data.pick_random())
-	
--- misc/traffic/vehicle_visibility_manager.gd
+++ misc/traffic/vehicle_visibility_manager.gd
@@ -1,11 +1,27 @@
 extends Node
 class_name VehicleVisibilityManager
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var movement_path: Path2D
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var prev_state: Dictionary = {}
+
+
+# :) This is much better than the old system. Could propably be optimized further
+func _physics_process(_delta: float) -> void:
+	var lanes: Dictionary = { 0.0: [], 0.5: [], 1.0: [] }
+	
+	for vehicle: Vehicle in movement_path.get_children(): lanes[vehicle.get_nearest_lane()].append(vehicle)
+	
+	if lanes == prev_state: return
+	
+	var vehicle_z_index: int = 1
+	
+	for lane in lanes.keys():
+		lanes[lane].sort_custom(func(a: Vehicle, b: Vehicle): return a.progress < b.progress)
+		
+		for vehicle: Vehicle in lanes[lane]:
+			vehicle.z_index = vehicle_z_index
+			vehicle_z_index += 1
+	
+	prev_state = lanes
